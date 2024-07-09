76272afe59386ba7062037bb33415a1dea980d68
satoru-tak <satoru.takaira@gmail.com>
2024-07-10 00:02:06 +0900

    LinkやNavigationの設定

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,19 @@
+import { Link, Outlet } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <Link className="logo-container" to="/">
+        <div>Logo</div>
+      </Link>
+      <div className="nav-link-container">
+        <Link className="nav-link" to="/shop">
+          SHOP
+        </Link>
+      </div>
+      <Outlet />
+    </div>
+  );
+}
+
+export default Navigation;
