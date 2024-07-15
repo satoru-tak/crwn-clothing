a492c84357680c38b34b0aa1f15973ddee9a273b
satoru-tak <satoru.takaira@gmail.com>
2024-07-15 11:20:05 +0900

    firebaseでユーザー作成

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,12 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBY98ASxJzEk9T14PP-N8hNTevq0Swlea8",
@@ -23,5 +29,38 @@ provider.setCustomParameters({
   prompt: "select_account",
 });
 
+// Googleサインインポップアップを呼び出す
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+// dbを取得
+export const db = getFirestore();
+
+// dbの値を取得
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid)
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  // user作成処理
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+}
